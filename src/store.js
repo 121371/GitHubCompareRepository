8bf9ba42141e41c3c303d6701d6f42c9aa3575ed
Amit Pathak <amit.pathak@publicissapient.com>
2020-09-09 23:51:04 +0530

    heroku config

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from './node_modules/redux';
 import createSagaMiddleware from './node_modules/redux-saga';
-import { fork } from './node_modules/redux-saga/effects';
-import "./node_modules/regenerator-runtime/runtime";
+import { fork } from 'redux-saga/effects';
+import "regenerator-runtime/runtime";
 
 import rootReducer from './Redux/Reducer/reducer';
 import { watchGetProfileSummary } from './Redux/Saga/saga';
